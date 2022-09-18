12fab90524ddbe32d13d8efbddc21fa520cd8408
Menoru <menoru.da@gmail.com>
2022-09-18 10:33:15 -0600

    Comprobación de inicio de sesión.

--- src/admin/header.php
+++ src/admin/header.php
@@ -1,3 +1,10 @@
+<?php
+require_once '../connect.php';
+
+session_start();
+
+if (empty($_SESSION['type']) || $_SESSION['type'] != 'Administrador') {header('location: /');}
+?>
 <!DOCTYPE html>
 <html lang="es-MX">
 
